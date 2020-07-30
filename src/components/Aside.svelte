cf32248c8023fc0d8eeb1bc4b9d09e595ec0e95d
Tim Smith <tim.smith.hdg@gmail.com>
2020-07-30 10:18:15 -0700

    new pages

--- src/components/Aside.svelte
+++ src/components/Aside.svelte
@@ -4,6 +4,20 @@
   export let images;
 </script>
 
+<style>
+  aside {
+    position: absolute;
+    top: 0;
+    left: 40px;
+    width: 453px;
+    height: 100%;
+    padding-top: 424px;
+    padding-left: 43px;
+    background-color: var(--hotPink_75);
+    border-right: 3px solid var(--darkerTeal);
+  }
+</style>
+
 <aside>
   <RotatingImages {images} speed={3000} />
 </aside>
